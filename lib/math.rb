705c97635f2dcd9ea937d445a906401b57fc5d0b
Colton Kaiser <kaiserc2@seattleu.edu>
2020-03-28 05:15:34 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,21 +4,24 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  dif = num1 - num2
+  dif
 end
 
 def division(num1, num2)
-
+  quot = num1 / num2
+  quot
 end
 
 def multiplication(num1, num2)
-
+  product = num1 * num2
+  product
 end
 
 def modulo(num1, num2)
-
+  remainder = num1 % num2
 end
 
 def square_root(num)
-
+  
 end
